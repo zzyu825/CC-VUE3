78b3df26da014bbc940363374b7f2c5d20271709
zhengzhiyu <zhengzhiyu825@163.com>
2020-10-17 16:37:20 +0800

    1-03

--- vue3-app-vite/src/App.vue
+++ vue3-app-vite/src/App.vue
@@ -1,49 +1,76 @@
 <template>
-  <h1>count: {{ countRef }}</h1>
-  <p>
-    <!-- <button @click="count++">count: {{ count }}</button> -->
-    <button @click="increase">increase</button>
-    <button @click="decrease">decrease</button>
-  </p>
+  <div id="app">
+    <section class="todoapp">
+      <header class="header">
+        <h1>todos</h1>
+        <input
+          class="new-todo"
+          autofocus=""
+          autocomplete="off"
+          placeholder="What needs to be done?"
+          v-model="newTodoRef"
+          @keyup.enter="addTodo"
+        />
+      </header>
+      <section class="main">
+        <input id="toggle-all" class="toggle-all" type="checkbox" />
+        <label for="toggle-all">Mark all as complete</label>
+        <ul class="todo-list">
+          <li class="todo">
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>学习composition api</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" type="text" />
+          </li>
+          <li class="todo">
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>投递50封简历</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" type="text" />
+          </li>
+          <li class="todo">
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>上午10:30 参加面试</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" type="text" />
+          </li>
+        </ul>
+      </section>
+      <footer class="footer">
+        <span class="todo-count">
+          <strong>3</strong>
+          <span>items left</span>
+        </span>
+        <ul class="filters">
+          <li><a href="#/all" class="selected">All</a></li>
+          <li><a href="#/active" class="">Active</a></li>
+          <li><a href="#/completed" class="">Completed</a></li>
+        </ul>
+        <button class="clear-completed" style="display: none">
+          Clear completed
+        </button>
+      </footer>
+    </section>
+  </div>
 </template>
 
 <script>
-import { ref } from "vue";
-
-function useCount() {
-  let countRef = ref(0);
-  const increase = () => {
-    countRef.value++;
-  };
-  const decrease = () => {
-    countRef.value--;
-  };
-  return {
-    countRef,
-    increase,
-    decrease
-  };
-}
+import useTodoList from "./composition/useTodoList";
+import useNewTodo from "./composition/useNewTodo";
 
 export default {
   setup() {
-    // console.log("所有生命周期钩子函数之前调用");
-    // console.log(this); // this -> undefined
-    // let count = ref(0);
-    // 在setup中，count是一个对象
-    // 在实例代理中，count是一个count.value
-    // console.log(count);
-    // const increase = () => {
-    //   count.value++;
-    // };
-    // return {
-    //   count,
-    //   increase
-    // };
-
+    const { todosRef } = useTodoList();
     return {
-      ...useCount()
-    };
-  },
+      todosRef,
+      ...useNewTodo(todosRef)
+    }
+  }
 };
 </script>
