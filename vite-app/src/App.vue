5d61c9c5b72c0eefbe3fc71f778f7b55454e29c4
zhengzhiyu <zhengzhiyu825@163.com>
2020-11-18 14:49:56 +0800

    2-02

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div class="menu-bar-container">
+    <div class="logo">
+      <h1>logo</h1>
+    </div>
+    <ul class="nav">
+      <li><a href="">menu</a></li>
+      <li><a href="">menu</a></li>
+      <li><a href="">menu</a></li>
+      <li><a href="">menu</a></li>
+      <li><a href="">menu</a></li>
+    </ul>
+    <div class="user" :class="user.name">
+      <span :class="user.name">{{ user.name }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +23,13 @@ export default {
   name: "App",
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      user: {
+        name: "monica",
+      },
+    };
   },
 };
 </script>
